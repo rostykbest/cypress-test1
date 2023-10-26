574473cec6a1fda8c169941522a428c7524ea3b2
rstan <rostykbest@gmail.com>
2023-10-26 17:30:11 +0300

    1111

--- cypress/e2e/smart-table/table1.cy.js
+++ cypress/e2e/smart-table/table1.cy.js
@@ -32,11 +32,19 @@ describe("add user", () => {
       .click()
       .type("25");
     cy.get("a.ng2-smart-action-add-create").should("be.visible").click();
-  });
 
-  it("should check data of the created user", () => {
-    cy.get(
-      "tbody[ng2-st-tbody] tr.ng2-smart-row td.ng-star-inserted div.ng-star-inserted"
-    ).contains("999");
+    // should check if the values are there
+
+    cy.get("table tbody tr.ng-star-inserted td")
+      .filter(
+        ':contains("999"), :contains("Ross"), :contains("Geller"), :contains("rossgeller"), :contains("rossgeller999@gmail.com"), :contains("24")'
+      )
+      .should("have.length.gt", 0, { timeout: 10000 })
+      .last()
+      .parent()
+      .find("a.ng2-smart-action-edit-edit")
+      .click();
   });
 });
+
+// should edit the values and save
